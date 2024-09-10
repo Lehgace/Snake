32f777c7f36c084bbe6e22d0f63a5112bbed2848
Josh <joshmerickson19@gmail.com>
2024-09-09 18:10:51 -0700

    Modified poker themed assets, faster snake decay for longer snakes

--- Snake/Images.cs
+++ Snake/Images.cs
@@ -7,18 +7,19 @@ namespace Snake
     // Container for all image assets
     public static class Images
     {
-        public readonly static ImageSource Empty = LoadImage("Empty.png");
+        //public readonly static ImageSource Empty = LoadImage("Empty.png");
         //public readonly static ImageSource Body = LoadImage("Body.png");
         //public readonly static ImageSource Head = LoadImage("Head.png");
         //public readonly static ImageSource Food = LoadImage("Food.png");
         //public readonly static ImageSource DeadBody = LoadImage("DeadBody.png");
         //public readonly static ImageSource DeadHead = LoadImage("DeadHead.png");
-        
-        public readonly static ImageSource Food = LoadImage("PokerChip.png");
+
+        public readonly static ImageSource Empty = LoadImage("EmptyWhite.png");
+        public readonly static ImageSource Food = LoadImage("RedPokerChip.png");
         public readonly static ImageSource Body = LoadImage("HandBody.png");
-        public readonly static ImageSource Head = LoadImage("HandHead.png");
-        public readonly static ImageSource DeadBody = LoadImage("DeadHandBody.png");
-        public readonly static ImageSource DeadHead = LoadImage("DeadHandHead.png");
+        public readonly static ImageSource Head = LoadImage("BlueCardBackground.png");
+        public readonly static ImageSource DeadBody = LoadImage("BlueCardBackground.png");
+        public readonly static ImageSource DeadHead = LoadImage("BlueCardBackground.png");
 
         private static ImageSource LoadImage(string fileName)
         {
